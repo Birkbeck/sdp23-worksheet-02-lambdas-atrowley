29ad55662228d37149afa7fed14d4f75a60d0f68
atrowley <atrowley81@gmail.com>
2023-02-12 15:19:44 +0000

    Q1

--- src/Outline.java
+++ src/Outline.java
@@ -1,9 +1,58 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1,7,3,4,8,2};
-    System.out.println(Arrays.asList(intArray));
-    // Arrays.sort(intArray,.......)
-  } 
+
+
+  public static String[] getAnimals () {
+      String[] animals = {"Dog","Cat","Bird","Frog","Crocodile","Billy goat gruff"};
+      return animals;
+  }
+
+
+    public static void Q1_1() {
+        String[] animals = getAnimals();
+        Arrays.sort(animals, (String a, String b) -> Integer.compare(a.length(), b.length()));
+        System.out.println(Arrays.asList(animals));
+    }
+
+    public static void Q1_2() {
+        String[] animals = getAnimals();
+        Arrays.sort(animals, (String a, String b) -> Integer.compare(b.length(), a.length()));
+        System.out.println(Arrays.asList(animals));
+    }
+
+    public static void Q1_3() {
+        String[] animals = getAnimals();
+        Arrays.sort(animals, (String a, String b) -> Character.compare(a.charAt(0), b.charAt(0)));
+        System.out.println(Arrays.asList(animals));
+    }
+
+    public static void Q1_4() {
+        String[] animals = getAnimals();
+        Arrays.sort(animals, (String a, String b) -> {
+            if (a.contains("e") && !b.contains("e")) {
+                return -1;
+            } else if (!a.contains("e") && b.contains("e")){
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        System.out.println(Arrays.asList(animals));
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Q1_1();
+        Q1_2();
+        Q1_3();
+        Q1_4();
+    }
+
+
+
+
 }
